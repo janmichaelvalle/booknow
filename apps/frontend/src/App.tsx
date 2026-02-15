256ec6eccf0eaf7aef1c7be23068460b2e69f69f
Mikee Valle <janmichaelvalle@gmail.com>
2026-02-15 17:46:41 +0800

    Added apis in our session

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import {Routes, Route} from "react-router-dom"
 import { QuotationPage } from "./pages/QuotationPage"
 import { ReservationPage } from "./pages/ReservationPage"
+import { useEffect } from "react"
 
 
 function App() {
 
+  useEffect(() => {
+    fetch("http://localhost:3000/api/reservation", {
+      method: "POST",
+      headers: {
+        type: "application/json"
+      },
+      body: JSON.stringify({
+        
+      })
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+  }, []) // initial load
 
   return (
      <Routes>
